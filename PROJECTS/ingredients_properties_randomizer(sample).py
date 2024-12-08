591ba8c42fa6b5abc519da6e49851fdf364b6481
Kostiantyn <klenets82@gmail.com>
2024-12-09 00:14:59 +0200

    Uploaded PROJECTS folder with files for 'Alchemy' learning project

---
+++ PROJECTS/ingredients_properties_randomizer(sample).py
@@ -0,0 +1,54 @@
+from pymongo import MongoClient
+import random
+
+# Підключення до бази даних
+client = MongoClient(
+    "mongodb+srv://Buagilber:e6O1Jlvqi0t3sW8r@learningcluster.pasif.mongodb.net/?retryWrites=true&w=majority")
+db = client["MyLearningProject"]
+
+# Визначення порядкового номеру для назви колекції ingredients_list
+collection_count = len([name for name in db.list_collection_names(
+) if name.startswith("ingredients_list_")]) + 1
+
+# Отримання даних з колекцій
+ingredients = db.ingredients.find()
+properties = db.properties.find()
+
+# Перетворення курсорів в списки
+ingredients_list = list(ingredients)
+temp_properties_list = list(properties)
+
+# Прибираємо зайві атрибути властивостей
+properties_list = [[item['_id'], item['multiplier']]
+                   for item in temp_properties_list]
+
+
+def get_random_property_id(properties_list):
+    # Вибирає випадковий ID властивості з урахуванням multiplier
+    weights = [property[1] for property in properties_list]
+    return random.choices(properties_list, weights=weights)[0][0]
+
+
+def assign_property_id(ingredients_list, properties_list):
+    # Присвоює кожному інгредієнту 4 унікальних ID властивостей
+    for ingredient in ingredients_list:
+        ingredient['property_ids'] = set()
+        while len(ingredient['property_ids']) < 4:
+            property_id = get_random_property_id(properties_list)
+            ingredient['property_ids'].add(property_id)
+
+
+# Викликаємо функцію для наповнення інгредієнтів властивостями
+assign_property_id(ingredients_list, properties_list)
+
+# Змінюємо формат значення властивостей для експорта в БД
+for ingredient in ingredients_list:
+    ingredient['property_ids'] = list(ingredient['property_ids'])
+
+# Отримуємо нову назву для колекції
+new_collection_name = f"ingredients_list_{collection_count}"
+
+# Створюємо колекцію інгредієнтів з властивостями
+collection = db[new_collection_name]
+collection.insert_many(ingredients_list)
+print(f"New collection {new_collection_name} was added to {db.name}")
